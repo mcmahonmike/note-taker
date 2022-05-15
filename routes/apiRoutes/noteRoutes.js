429e291b21a60e771d24953695d1aa105bac62dc
Hephaestus <mcmahonmike9@yahoo.com>
2022-05-15 15:31:46 -0500

    POST and GET requests built

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,12 @@
+// const { notes } = require('./db/db.json')
+// const router = require('express').Router();
+
+
+// router.get('/notes', (req, res) => {
+//     res.json(notes)
+// })
+
+// router.post('/notes', (req, res) => {
+//     console.log(req.body);
+//     res.json(req.body);
+// })
--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 const fs = require('fs');
+const path = require('path')
 const express = require('express');
 const { notes } = require('./db/db.json')
+
+let newNotes = []
+
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// app.get('/notes', (req, res) => {
+//     res.json(notes)
+// })
+
+app.post('/notes', (req, res) => {
+    
+    let notesObject = {
+        title: req.body.title,
+        text: req.body.text,
+        id: JSON.stringify(newNotes.length)
+    };
+    newNotes.push(notesObject)
+
+    // fs.writeFileSync(
+    //     path.join(__dirname, './db/db.json'),
+    //     JSON.stringify({ notes: newNotes })
+    // );
+    
+    res.json(req.body);
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
 
 
 app.listen(PORT, () => {
