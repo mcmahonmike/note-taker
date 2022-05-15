4de13eb3340596448026221b207d30e3de996b8e
Hephaestus <mcmahonmike9@yahoo.com>
2022-05-15 12:22:45 -0500

    add heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const express = require('express');
+const { notes } = require('./db/db.json')
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
